70e34ce72aea6dd6bb11abad04861f3c308c3aa4
admin <389956919@qq.com>
2020-09-24 12:09:11 +0800

    换电脑测试

--- src/main/java/com/gyb/test/JvmTest.java
+++ src/main/java/com/gyb/test/JvmTest.java
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
-public class JvmTest  implements  Lock{
+public class JvmTest {
     private static int M = 1024*1024;
 
     private static final Integer K =  1024;
@@ -30,33 +30,5 @@ public class JvmTest  implements  Lock{
 
     }
 
-    @Override
-    public void lock() {
 
-    }
-
-    @Override
-    public void lockInterruptibly() throws InterruptedException {
-
-    }
-
-    @Override
-    public boolean tryLock() {
-        return false;
-    }
-
-    @Override
-    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-        return false;
-    }
-
-    @Override
-    public void unlock() {
-
-    }
-
-    @Override
-    public Condition newCondition() {
-        return null;
-    }
 }
